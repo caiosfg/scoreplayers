fdf155d23c07504bf87280634ec64b9bb7759d06
caiosfg <caio.sfg@gmail.com>
2024-03-03 23:56:00 -0300

    all the routes

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
@@ -20,6 +21,11 @@ class Player(BaseModel):
     name: str
     age: int
     team: str
+
+class UpdatePlayer(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    team: Optional[str] = None
 
 @app.get("/")
 def team():
@@ -42,6 +48,20 @@ def post_player(player_id: int, player: Player):
         return { "Erro" : "Player registered before" }
     players[player_id] = player
     return players[player_id]
+
+@app.put("/player-update/{id_player}")
+def put_player(player_id: int, player: UpdatePlayer):
+    if player_id not in players:
+        return { "Erro" : "Player not registered" }
+    if player.name != None:
+        players[player_id]["name"] = player.name
+    if player.age != None:
+        players[player_id]["age"] = player.age
+    if player.team != None:
+        players[player_id]["team"] = player.team
+    
+    return players[player_id]
+    
 
 @app.delete("/player-excluded/{id_player}")
 def exclude_player(player_id: int):
